43272fc37292e3fab70a6c648e37f432e196156c
Seshasen <132787325+Seshasen@users.noreply.github.com>
2023-08-11 19:28:23 +0100

    Update run_dbt.sh

--- dbt_redshift/run_dbt.sh
+++ dbt_redshift/run_dbt.sh
@@ -2,31 +2,31 @@
 
 set -e
 
-echo "Running .py script to get glue connection redshift credentials"
+echo "Running python script to retreive redshift connection params"
 python3 ./export_redshift_connection.py 
 
 echo "Exporting Redshift credentials as environment variables to be used by dbt"
 . ./.redshift_credentials
 
 echo "Running DBT commands"
-echo "Execute all dependencies"
+echo "Run all dependencies to collect the required packages before dbt model execution"
 dbt deps --profiles-dir . --project-dir .
 
-echo "Processing seed files to redshift"
+echo "Run seed operation to load static data to redshift"
 dbt seed --profiles-dir . --project-dir .
 
-echo "Execute all model files to redshift"
+echo "Run all model files holding business logic"
 dbt run --profiles-dir . --project-dir .
 
-echo "Execute snapshot tables queries"
+echo "Run snapshot queries for SCD type2"
 dbt snapshot --profiles-dir . --project-dir .
 
-echo "Run all predefined test cases"
+echo "Run all predefined test cases, Test cases can be configured to warn or error"
 dbt test --profiles-dir . --project-dir .
 
-echo "Generate documentation"
+echo "Generate documentation files"
 dbt docs generate --profiles-dir . --project-dir .
 echo ""
 
-echo "Copying dbt outputs to s3 bucket for hosting"
+echo "Copying dbt documentation files for hosting"
 aws s3 cp --recursive --exclude="*" --include="*.json" --include="*.html" dbt/target/ s3://<BucketName>/REDSHIFT_POC/
